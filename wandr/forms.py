79aae005c9a3ea689585b3baf97dd18be35caf16
Cynthia Lee <cynthia.lee@gmx.de>
2017-02-28 14:15:18 +0000

    Picture form added

--- wandr/forms.py
+++ wandr/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from wandr.models import UserProfile
+from wandr.models import Picture
 
+#Cristina:
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
 
@@ -13,3 +15,15 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('picture',)
+
+#Cynthia:
+class PictureForm(forms.ModelForm):
+	name = forms.CharField(max_length=128, required=True, help_text="Name your picture.")
+	description = models.CharField(max_length=200, required=True, help_text="Describe your picture in a short sentence")
+	likes = forms.IntegerField(widget=forms.HiddenInput(), initial = 0)
+	slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+	picture = forms.ImageField(upload_to='profile_images', required=True)
+
+	class Meta:
+		model = Picture
+		fields('name', 'description', 'picture')
