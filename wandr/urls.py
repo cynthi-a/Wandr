8336bff7ad0061817a912d744cb8ba4b81f4261c
cri-zrh <cri-zrh>
2017-03-02 09:34:51 +0000

    add about and contact page

--- wandr/urls.py
+++ wandr/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^logout/$', views.user_logout, name='logout')
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^contact/$', views.contact, name='contact')
 ]
