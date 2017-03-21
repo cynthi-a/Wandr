eb8ac4b0305784485ee3290e02c6ba8182679ae6
Rob-Aws <RMac@glaroam2-163-177.wireless.gla.ac.uk>
2017-03-21 16:42:11 +0000

    Added javascript for the menu transition animation

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,19 @@
 /**
  * Created by RMac on 28/02/2017.
  */
+
+function menuContraction(){
+	var mq = window.matchMedia( "(min-width: 992px)" );
+	if (mq.matches) {
+  		if(window.scrollY > '50'){
+  			document.getElementById("menu-bar").style.padding = '0px';
+  		}
+  		else {
+  			document.getElementById("menu-bar").style.padding = '20px 0px';
+  		}
+	}
+
+}
+
+window.onload = menuContraction;
+window.addEventListener("scroll", menuContraction);
