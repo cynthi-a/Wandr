de8069aaddf3a6bfd4a9733def44a9270cc361c0
Rob-Aws <RMac@Roberts-MacBook-Air.local>
2017-03-22 13:54:57 +0000

    test file started

--- wandr/tests.py
+++ wandr/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from wandr.models import Picture, HaveBeenList
 
-# Create your tests here.
+
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        """
+        ensure_views_are_positive should results True for categories where views are zero or positive
+        """
+        hbl = HaveBeenList.objects.get(belongs_to='')
+        pic = Picture(name='test', location='test', description='test', likes=-1, have_been_list=HaveBeenList)
+        pic.save()
+        self.assertEqual((pic.likes >= 0), True)
+
+
+def login_client_user(self):
+    self.client.login(username='admin', password='secret666')
+    return self
+
+
+def logout_client_user(self):
+    self.client.logout()
+    return self
